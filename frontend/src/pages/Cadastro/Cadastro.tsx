c829145276ee47c5a4697f3e7c88a7ec4c9b2c29
Pedro-areis <pedrogiarola1@gmail.com>
2026-01-14 19:13:59 -0300

    refact: refatora código css da tela de cadastro com tailwind

--- frontend/src/pages/Cadastro/Cadastro.tsx
+++ frontend/src/pages/Cadastro/Cadastro.tsx
@@ -1,24 +1,35 @@
-import "../Login/Login.css"
 import InitialPage from "../../components/InitialPage/InitialPage"
 import { Link } from "react-router-dom"
 
 function RegisterPage() {
     return (
         <InitialPage title="Cadastre-se">
-            <form>
-                <img className="iconsImg" src="src\assets\User.png" alt="userImg" />
-                <input type="text" placeholder="Nome Completo" />
+            <form className="flex flex-col justify-center items-center">
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\User.png" alt="userImg" />
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 font-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="text" placeholder="Nome Completo" />
 
-                <img className="iconsImg" src="src\assets\At sign.png" alt="userImg" />
-                <input type="email" placeholder="E-mail"/>
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\At sign.png" alt="userImg" />
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 font-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="email" placeholder="E-mail"/>
 
-                <img className="iconsImg" src="src\assets\Calendar.png" alt="userImg" />
-                <input type="date" placeholder="Data de Nascimento"  />
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\Calendar.png" alt="userImg" />
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 font-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="date" placeholder="Data de Nascimento"  />
 
-                <img className="iconsImg" src="src\assets\Lock.png" alt="userImg" />
-                <input type="password" placeholder="Senha"/>
-                <button type="submit">Login</button>
-                <p>Já possui uma conta? <Link to='/login'>Entre</Link></p>
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\Lock.png" alt="userImg" />
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 font-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="password" placeholder="Senha"/>
+
+                <button className="mt-16.25 bg-[#ffe100] cursor-pointer border border-transparent
+                rounded-[5px] p-1.25 w-42.5 h-8.75 hover:bg-[#000000]
+                hover:text-[#ffe100] hover:border-[#ffe100]" type="submit">Login</button>
+                
+                <p className="relative top-12.5 text-white">Já possui uma conta? <Link className="text-blue-500 hover:underline hover:text-blue-600" to='/login'>Entre</Link></p>
             </form>
         </InitialPage>
     )
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,4 +1,3 @@
-import "./Login.css"
 import InitialPage from "../../components/InitialPage/InitialPage"
 import { Link } from "react-router-dom"
 
@@ -6,13 +5,23 @@ function LoginPage() {
     
     return (
         <InitialPage title="Bem vindo a sua Web Wallet!">
-            <form>
-                <img className="iconsImg" src="src\assets\User.png" alt="userImg" />
-                <input type="email" placeholder="E-mail"/>
-                <img className="iconsImg" src="src\assets\Lock.png" alt="userImg" />
-                <input type="password" placeholder="Senha"/>
-                <button type="submit">Login</button>
-                <p>Não tem uma conta? <Link to='/register'>Cadastre-se</Link></p>
+            <form className="flex flex-col justify-center items-center">
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\User.png" alt="userImg" />
+                
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 font-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="email" placeholder="E-mail"/>
+
+                <img className="relative right-40 top-8.75 w-8.75 h-8.75" src="src\assets\Lock.png" alt="userImg" />
+                
+                <input className="bg-[#ffe100] border border-transparent rounded-[5px] 
+                p-1.25 w-67.5 h-8.75 text-[18px] outline-none hover:bg-[#000000] hover:text-[#ffe100]
+                hover:border-[#ffe100]" type="password" placeholder="Senha"/>
+
+                <button className="mt-16.25 bg-[#ffe100] cursor-pointer border border-transparent
+                rounded-[5px] p-1.25 w-42.5 h-8.75 hover:bg-[#000000]
+                hover:text-[#ffe100] hover:border-[#ffe100]" type="submit">Login</button>
+                <p className="relative top-12.5 text-white">Não tem uma conta? <Link className="text-blue-500 hover:underline hover:text-blue-600" to='/register'>Cadastre-se</Link></p>
             </form>
         </InitialPage>
     )
