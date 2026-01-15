02959f43fefba159ba8449621ea6cbd95875b971
Pedro-areis <pedrogiarola1@gmail.com>
2026-01-15 20:46:31 -0300

    feat: adiciona botões de navageação ao navbar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -8,11 +8,12 @@ function NavBar() {
     }
 
     return (
-        <nav className="flex flex-col w-87.5 h-screen bg-[#000000] rounded-[20px] 
+        <nav className="flex flex-col w-92.5 h-screen bg-[#000000] rounded-[20px] 
         p-5 border border-[#ffe100]">
             <div className="flex flex-row justify-between items-center mt-10
             w-full h-25">
-                <div className="flex items-center justify-center w-25 h-25 rounded-[100%] bg-[#ffe100]">
+                <div className="flex items-center justify-center w-25 h-25 
+                rounded-[100%] bg-[#ffe100] m-2.5">
                     <img src="src\assets\UserBlack.png" alt="perfil" />
                 </div>
                 <div className="flex flex-col items-center w-[50%] h-full justify-around">
@@ -22,6 +23,19 @@ function NavBar() {
                     <button onClick={handleLogout} className="flex w-32.5 h-10 p-1.25 font-semibold text-black bg-[#ffe100]
                     rounded-[5px] items-center justify-center cursor-pointer hover:bg-[#dfc543]">Sair</button>
                 </div>
+            </div>
+            <div className="flex flex-col mt-42.5 gap-8 items-center justify-center">
+                <button className="flex w-67.5 h-14 p-1.25 font-semibold text-black bg-[#ffe100]
+                rounded-[5px] items-center justify-center cursor-pointer hover:bg-[#dfc543]">Home</button>
+
+                <button className="flex w-67.5 h-14 p-1.25 font-semibold text-black bg-[#ffe100]
+                rounded-[5px] items-center justify-center cursor-pointer hover:bg-[#dfc543]">Carteira</button>
+                
+                <button className="flex w-67.5 h-14 p-1.25 font-semibold text-black bg-[#ffe100]
+                rounded-[5px] items-center justify-center cursor-pointer hover:bg-[#dfc543]">Transações</button>
+                
+                <button className="flex w-67.5 h-14 p-1.25 font-semibold text-black bg-[#ffe100]
+                rounded-[5px] items-center justify-center cursor-pointer hover:bg-[#dfc543]">Configurações</button>
             </div>
         </nav>
     )
