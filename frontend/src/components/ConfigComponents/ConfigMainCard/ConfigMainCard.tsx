a00ce2879e40c05b4a80f272b258398c44acbc86
Pedro-areis <pedrogiarola1@gmail.com>
2026-01-20 10:57:27 -0300

    feat: cria formulario para alterar dados do usuário (e-mail e senha)

--- frontend/src/components/ConfigComponents/ConfigMainCard/ConfigMainCard.tsx
+++ frontend/src/components/ConfigComponents/ConfigMainCard/ConfigMainCard.tsx
@@ -1,7 +1,15 @@
-function ConfigMainCard() {
+import type { ReactNode } from "react";
+
+interface ConfigMainCardProps {
+    title: string;
+    children: ReactNode;
+}
+
+function ConfigMainCard({title, children}: ConfigMainCardProps) {
     return (
-        <article className="flex w-full h-full bg-[#ffe100] rounded-[20px]">
-            
+        <article className="flex flex-col w-full h-full bg-[#ffe100] rounded-[20px] p-3">
+            <h1 className="text-3xl w-full text-center font-bold border-b pb-2">{title}</h1>
+            {children}
         </article>
     );
 }
--- frontend/src/pages/UserConfig/UserConfig.tsx
+++ frontend/src/pages/UserConfig/UserConfig.tsx
@@ -9,7 +9,29 @@ function UserConfig() {
             <NavBar />
             <div className="flex flex-row w-[80%] h-full items-center justify-around">
                 <section className="flex w-[50%] h-full">
-                    <ConfigMainCard />
+                    <ConfigMainCard title="Configurações do Perfil">
+                        <div className="flex flex-row w-full h-full p-4">
+                            <div className="flex flex-col w-[50%] h-full gap-4">
+                                <p className="text-2xl font-semibold">Bem vindo Usuário!</p>
+                                <form className="flex flex-col">
+                                    <label htmlFor="email">
+                                        <img src="src\assets\At sign_Black.png" alt="" />
+                                    </label>
+                                    <input className="flex relative left-10 bottom-8 w-62.5 h-8 bg-[#0F0E0D] rounded-[7px]
+                                    outline-none p-2 text-white items-center" type="email" id="email" placeholder="emaildousuario@gmail.com" />
+                                    
+                                    <label htmlFor="password">
+                                        <img src="src\assets\LockBlack.png" alt="" />
+                                    </label>
+                                    <input className="flex relative left-10 bottom-8 w-62.5 h-8 bg-[#0F0E0D] rounded-[7px]
+                                    outline-none p-2 text-white items-center" type="password" id="password" placeholder="Nova senha" />
+                                </form>
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>
+                    </ConfigMainCard>
                 </section>
                 <section className="flex flex-col w-[45%] h-full items-center justify-between">
                     <ConfigTopCard />
